965ffe86eab8a231bf9de27e8a253b019a35647a
dayat@Bounty <stu2501261018@uni-plovdiv.bg>
2026-04-10 21:16:08 +0300

    problem 3 solved

---
+++ Lecture5/src/SumNumbers.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class SumNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int number = Integer.parseInt(sc.nextLine());
+        int input = 0;
+        int sum = 0;
+
+        while(sum < number){
+
+            input = Integer.parseInt(sc.nextLine());
+            sum += input;
+        }
+
+        System.out.println(sum);
+    }
+}
